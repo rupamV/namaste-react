e43a169be175f1a3cc89904ad312b3d7af82fab0
rupammV <rupammvallecha@outlook.com>
2024-04-19 23:23:45 +0530

    updated app 	modified:   package-lock.json modified:   package.json 	modified:   src/App.js 	new file:   src/components/About.js 	new file:   src/components/Cart.js 	new file:   src/components/Contact.js 	new file:   src/components/Error.js 	modified:   src/components/Header.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,49 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import Cart from "./components/Cart";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 
 const AppBody = () => {
   return(
       <div className="mainapp">
           <Header />
-          <Body />
+          <Outlet />
       </div> 
   )
 };
 
+const appRouter = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <AppBody />,
+      children: [
+        {
+          path: "/",
+          element: <Body />
+        },
+        {
+          path: "/about",
+          element: <About />
+        },
+        {
+          path: "/contact",
+          element: <Contact />
+        },
+        {
+          path: "/cart",
+          element: <Cart />
+        }
+      ],
+      errorElement: <Error />
+    },
+  ]
+);
 
 const root= ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppBody/>);
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About Component</h1>
+            <p>This is the About component.</p>
+        </div>
+    );
+};
+
+export default About;
