b6c2c849340f1f4b924837d36728800552be0066
rupammV <rupammvallecha@outlook.com>
2024-04-27 17:24:41 +0530

    modified:   index.css 	modified:   src/components/Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,6 +2,7 @@ const Cart = () => {
     return (
         <div>
             <h1>Cart</h1>
+            <p className="para">Your cart is empty. Add items to your cart to proceed to checkout.</p>
         </div>
     );
 }
