50ddb50a18ca6983e3f1ff98afd3a9889312ef54
rupammV <rupammvallecha@outlook.com>
2024-07-23 14:46:34 +0530

    modified:   src/components/Menu.js 	modified:   src/utils/constants.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,19 +10,19 @@ const Menu = () => {
     }, []);
 
     const fetchMenu = async () => {
-        const data = await fetch('https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.668395094474295&lng=77.18985289335252&restaurantId='+resId+'&catalog_qa=undefined&isMenuUx4=true&submitAction=ENTER');
+        const data = await fetch('https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.668395094474295&lng=77.18985289335252&restaurantId='+resId+'catalog_qa=undefined&submitAction=ENTER');
 
         const json = await data.json();
         setResInfo(json.data);
 
         if (json.data) {
-            const cards = json.data.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards;
+            const cards = json.data.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards;
             setItemCards(cards);
         }
     }
 
     
-    const { name, cuisines, costForTwoMessage } = resInfo?.cards[2]?.card?.card?.info;
+    const { name, cuisines = [], costForTwoMessage } = resInfo?.card?.info ?? {};
     
     return (
         <div className='menu bg-white shadow-lg rounded-lg p-6 m-4'>
